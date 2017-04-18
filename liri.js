b4dce943a993335baa274d142cd738fa52214f08
Boubacar Balde <boubacarbalde@Boubacars-MacBook-Pro.local>
2017-04-18 13:19:51 -0400

    finished that bonus

--- liri.js
+++ liri.js
@@ -3,11 +3,7 @@ var twitter_bouba = require("./keys.js");
 var twitterList = twitter_bouba.twitterKeys;
 
 
-
-// Loop through twitterlist and print out details
-// for (var key in twitterList) {
-//   console.log("A " + key + " band is " + twitterList[key] + ".");
-// }
+var filePath = '/Users/boubacarbalde/Desktop/Homework/liri-node-app/log.txt'; 
 
 
 var Twitter = require('twitter');
@@ -42,14 +38,23 @@ function the_twits()
 {
 	var client = new Twitter(twitterList);
 
+	fs.unlinkSync(filePath);
+
 		var params = {screen_name: 'balde_boubs'};
 		client.get('statuses/user_timeline', params, function(error, tweets, response) {
 		  if (!error) {
 		    //console.log(tweets);
 
 		    for (var i=0; i<4; i++){
-		    	console.log(tweets[i].text);
-		    	console.log("tweeted at :" + tweets[i].created_at);
+		   //console.log(tweets[i].text);
+		   //console.log("tweeted at :" + tweets[i].created_at);
+				   fs.appendFile("log.txt", tweets[i].text + "\n", (err, value) => {
+				   console.log(tweets[i].text);
+			   	 	});
+
+					fs.appendFile("log.txt", "tweeted at :" + tweets[i].created_at + "\n", (err, value) => {
+					console.log("tweeted at :" + tweets[i].created_at);
+			 		});
 		    }
 
 		  }
@@ -59,7 +64,9 @@ function the_twits()
 
 function the_spotify()
 {
+	fs.unlinkSync(filePath);
 	var that_song = process.argv[3];
+
 
 	spotify.search({ type: 'track', query: that_song }, function(err, data) {
     if ( err ) {
@@ -71,10 +78,26 @@ function the_spotify()
     var the_output = data.tracks.items;
     for (var i=0; i<the_output.length; i++)
     {
-    console.log("Artist(s):" + " " +the_output[i].artists[0].name);
-    console.log("The album that the song is from:" + " " + the_output[i].album.name);
-    console.log("The preview link of the song from Spotify:" + " " + the_output[i].album.external_urls.spotify);
-    console.log("The song's name:" + " " + the_output[i].name);
+    	console.log("Artist(s):" + " " +the_output[i].artists[0].name);	 
+    	fs.appendFile("log.txt", the_output[i].artists[0].name + "\n", (err, value) => {
+			 		});
+
+		console.log("The album that the song is from:" + " " + the_output[i].album.name);	
+		fs.appendFile("log.txt", "The album that the song is from:" + " " + the_output[i].album.name+ "\n", (err, value) => {
+					});
+
+		console.log("The preview link of the song from Spotify:" + " " + the_output[i].album.external_urls.spotify);
+	    fs.appendFile("log.txt", "The preview link of the song from Spotify:" + " " + the_output[i].album.external_urls.spotify+ "\n", (err, value) => {
+					});
+	   
+	   console.log("The song's name:" + " " + the_output[i].name); 
+	   fs.appendFile("log.txt", "The song's name:" + " " + the_output[i].name+ "\n", (err, value) => {
+		 
+			   	 	});
+    	
+    
+			   	 	
+   
 }
 
 });
@@ -83,6 +106,7 @@ function the_spotify()
 
 function the_movie()
 {
+	fs.unlinkSync(filePath);
 	// Store all of the arguments in an array
 var nodeArgs = process.argv;
 
@@ -129,17 +153,42 @@ request(queryUrl, function(error, response, body) {
   if (!error && response.statusCode === 200) {
 
    
-    // (Note: The syntax below for parsing isn't obvious. Just spend a few moments dissecting it).
-    //console.log("Release Year: " + JSON.parse(body).Year);
+  
     console.log("* Title of the movie:" +" "+ JSON.parse(body).Title);
-    console.log("* Year the movie came out:" + " "+ JSON.parse(body).Year);
-    console.log("* IMDB Rating of the movie:" + " "+ JSON.parse(body).imdbRating);
+	fs.appendFile("log.txt", "* Title of the movie:" +" "+ JSON.parse(body).Title+ "\n", (err, value) => {
+			 		});
+
+	console.log("* Year the movie came out:" + " "+ JSON.parse(body).Year);
+    fs.appendFile("log.txt", "* Year the movie came out:" + " "+ JSON.parse(body).Year + "\n", (err, value) => {
+			 		});
+
+	console.log("* IMDB Rating of the movie:" + " "+ JSON.parse(body).imdbRating);
+    fs.appendFile("log.txt", "* IMDB Rating of the movie:" + " "+ JSON.parse(body).imdbRating+ "\n", (err, value) => {
+			 		});
+
     console.log("* Country where the movie was produced:" +" "+ JSON.parse(body).Title);
-    console.log("* Language of the movie:" +" "+ JSON.parse(body).Language);
+	fs.appendFile("log.txt", "* Country where the movie was produced:" +" "+ JSON.parse(body).Title + "\n", (err, value) => {
+			 		});
+
+	console.log("* Language of the movie:" +" "+ JSON.parse(body).Language);
+    fs.appendFile("log.txt", "* Language of the movie:" +" "+ JSON.parse(body).Language+ "\n", (err, value) => {
+			 		});
+
     console.log("* Plot of the movie:" +" "+ JSON.parse(body).Plot);
+	fs.appendFile("log.txt", "* Plot of the movie:" +" "+ JSON.parse(body).Plot+ "\n", (err, value) => {
+			 		});
+
     console.log("* Actors in the movie:" +" "+ JSON.parse(body).Actors);
+    fs.appendFile("log.txt", "* Actors in the movie:" +" "+ JSON.parse(body).Actors+ "\n", (err, value) => {
+			 		});
+
  	console.log("* Rotten Tomatoes Rating:" +" "+ JSON.parse(body).Ratings[1].Value);
-    console.log("* Rotten Tomatoes URL:" +" "+ JSON.parse(body).tomatoURL);
+ 	fs.appendFile("log.txt", "* Rotten Tomatoes Rating:" +" "+ JSON.parse(body).Ratings[1].Value+ "\n", (err, value) => {
+			 		});
+
+	console.log("* Rotten Tomatoes URL:" +" "+ JSON.parse(body).tomatoURL);
+	fs.appendFile("log.txt", "* Rotten Tomatoes URL:" +" "+ JSON.parse(body).tomatoURL+ "\n", (err, value) => {
+			 		});
 
   }
 });
@@ -148,7 +197,7 @@ request(queryUrl, function(error, response, body) {
 
 function the_says()
 {
-	
+	fs.unlinkSync(filePath);
 
 	fs.readFile("random.txt", "utf8", function(err, data) {
 	var to_play= data;
@@ -160,14 +209,28 @@ function the_says()
         return;
     }
 
-    //console.log(JSON.stringify(data, null, 2));
+    
     var the_output = data.tracks.items;
     for (var i=0; i<the_output.length; i++)
     {
-    console.log("Artist(s):" + " " +the_output[i].artists[0].name);
-    console.log("The album that the song is from:" + " " + the_output[i].album.name);
-    console.log("The preview link of the song from Spotify:" + " " + the_output[i].album.external_urls.spotify);
-    console.log("The song's name:" + " " + the_output[i].name);
+    	console.log("Artist(s):" + " " +the_output[i].artists[0].name);	 
+    	fs.appendFile("log.txt", the_output[i].artists[0].name + "\n", (err, value) => {
+			 		});
+
+		console.log("The album that the song is from:" + " " + the_output[i].album.name);	
+		fs.appendFile("log.txt", "The album that the song is from:" + " " + the_output[i].album.name+ "\n", (err, value) => {
+					});
+
+		console.log("The preview link of the song from Spotify:" + " " + the_output[i].album.external_urls.spotify);
+	    fs.appendFile("log.txt", "The preview link of the song from Spotify:" + " " + the_output[i].album.external_urls.spotify+ "\n", (err, value) => {
+					});
+	   
+	   console.log("The song's name:" + " " + the_output[i].name); 
+	   fs.appendFile("log.txt", "The song's name:" + " " + the_output[i].name+ "\n", (err, value) => {
+		 
+			   	 	});
+
+    
 }
 
 });
